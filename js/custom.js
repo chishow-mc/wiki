489a0fb6bba6d64b751062d09d4e052a2faec74a
eai04191 <eai@mizle.net>
2017-03-04 11:19:11 +0900

    コミットログリンク修正

--- js/custom.js
+++ js/custom.js
@@ -8,7 +8,7 @@ $(function(){
         $("h1").html($("h1").html()+'<small><a id="history"><i class="fa fa-history"></i></a></small>');
         var file = location.hash.slice(2);
         if (file.lastIndexOf("#") != -1) {file = file.slice(0,file.lastIndexOf("#"));}
-        var historyUrl = "https://github.com/chishow-mc/chishow-mc.github.io/commits/master/"+file;
+        var historyUrl = "https://github.com/chishow-mc/wiki/commits/master/"+file;
         $("#history").attr("href", historyUrl)
 
         // 上に戻るボタン
